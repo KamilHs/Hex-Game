4ecd0e2ec0f370f6318e8f33de77aa787d24fa4e
KamilHs <kamilsalimli1940@gmail.com>
2020-11-30 21:53:37 +0400

    dotenv added

--- db.js
+++ db.js
@@ -1,20 +1,13 @@
 const { Sequelize } = require("sequelize");
 
-// const sequelize = new Sequelize({
-//     host: process.env.DB_HOST,
-//     username: process.env.DB_USER,
-//     database: process.env.DB_DATABASE,
-//     password: process.env.DB_PASSWORD || "",
-//     dialect: "mysql",
-// });
-
 const sequelize = new Sequelize({
-    host: 'localhost',
-    username: 'root',
-    database: 'hex_game',
-    password: '',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD || "",
     dialect: "mysql",
 });
+
 
 
 module.exports = sequelize;
